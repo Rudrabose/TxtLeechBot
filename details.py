fef76973327b5be48e8b64d7430223c2462f8c87
Rudrabose <92892605+Rudrabose@users.noreply.github.com>
2024-04-21 08:08:59 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = 27881923
-api_hash = "79abda5e46a51fc0dce1313f2548ce19"
-bot_token = "6263666911:AAF_Zm2baKNCJMGPn8aCBlVArvTa-A4kz1k"
+api_id = 1324937
+api_hash = "e915776334fc44d2c86fa3be3cbf8552"
+bot_token = "6888175643:AAHwnygGymM-aEQHj79_6mChCzo1iChMA3U"
 
